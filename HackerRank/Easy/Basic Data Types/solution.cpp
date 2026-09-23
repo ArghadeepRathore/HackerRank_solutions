31ab452d80f4690134fc661fcd871a092929eae8
ArghadeepRathore <arghadeeprathore2432707@gmail.com>
2026-09-23 12:31:20 +0530

    Add HackerRank Easy - Basic Data Types

---
+++ HackerRank/Easy/Basic Data Types/solution.cpp
@@ -0,0 +1,21 @@
+// HackerRank Problem: Basic Data Types
+// Link: https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem
+// Difficulty: Easy
+// Language: cpp
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    
+    cin >> a >> b >> c >> d >> e;
+    
+    printf("%d\n%ld\n%c\n%f\n%lf\n", a, b, c, d, e);
+    return 0;
+}
