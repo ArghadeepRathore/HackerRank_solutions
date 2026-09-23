09d2ad5ff8c4098a1098529135297c750b4e00f8
ArghadeepRathore <arghadeeprathore2432707@gmail.com>
2026-09-23 12:31:48 +0530

    Add HackerRank Medium - Printing Tokens

---
+++ HackerRank/Medium/Printing Tokens/solution.c
@@ -0,0 +1,25 @@
+// HackerRank Problem: Printing Tokens
+// Link: https://www.hackerrank.com/challenges/printing-tokens-/problem
+// Difficulty: Medium
+// Language: c
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    //Write your logic to print the tokens of the sentence here.
+    for (char *c = s; *c != NULL; c++) {
+    if (*c == ' ') {
+        *c = '\n';
+    }
+}
+printf("%s", s);
+    return 0;
+}
