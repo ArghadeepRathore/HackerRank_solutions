05b94176fd88481d5d5ca3c6ff9b6f094f34a2c1
ArghadeepRathore <arghadeeprathore2432707@gmail.com>
2026-09-23 12:28:24 +0530

    Add HackerRank Easy - Grading Students

---
+++ HackerRank/Easy/Grading Students/solution.c
@@ -0,0 +1,24 @@
+// HackerRank Problem: Grading Students
+// Link: https://www.hackerrank.com/challenges/grading/problem
+// Difficulty: Easy
+// Language: c
+
+#include<stdio.h>
+int main(){
+    int n,i;
+    scanf("%d",&n);
+    int arr[n],remain;
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        remain=arr[i]%5;
+        if(arr[i]>=38){
+            if(arr[i]%5>2){
+              arr[i]=arr[i]+5-remain;
+            }
+        }
+    }
+    for(i=0;i<n;i++){
+        printf("%d\n",arr[i]);
+    }
+    return 0;
+}
