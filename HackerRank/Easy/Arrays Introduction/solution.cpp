13638af80b9b61fb370bb3e437fb6d02b0facf7f
ArghadeepRathore <arghadeeprathore2432707@gmail.com>
2026-09-23 12:30:47 +0530

    Add HackerRank Easy - Arrays Introduction

---
+++ HackerRank/Easy/Arrays Introduction/solution.cpp
@@ -0,0 +1,27 @@
+// HackerRank Problem: Arrays Introduction
+// Link: https://www.hackerrank.com/challenges/arrays-introduction/problem
+// Difficulty: Easy
+// Language: cpp
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n ;
+    cin >> n;
+    int a[n] ;
+    for(int i =  0 ; i<n ;i++)
+    {
+        cin >> a[i];
+    }
+    for(int i=n-1 ; i>=0 ; i--)
+    {
+        cout << a[i] << " ";
+    }   
+    return 0;
+}
