5aedc8056003837cb200a636d2130b6a39121ec1
ArghadeepRathore <arghadeeprathore2432707@gmail.com>
2026-09-23 12:33:02 +0530

    Add HackerRank Easy - Solve Me First

---
+++ HackerRank/Easy/Solve Me First/solution.c
@@ -0,0 +1,22 @@
+// HackerRank Problem: Solve Me First
+// Link: https://www.hackerrank.com/challenges/solve-me-first/problem
+// Difficulty: Easy
+// Language: c
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int solveMeFirst(int a, int b) {
+  return a+b;
+}
+int main() {
+  int num1,num2;
+  scanf("%d %d",&num1,&num2);
+  int sum; 
+  sum = solveMeFirst(num1,num2);
+  printf("%d",sum);
+  return 0;
+}
