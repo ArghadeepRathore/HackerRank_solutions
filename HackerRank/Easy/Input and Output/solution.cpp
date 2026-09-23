2f75f139162975a278d7fc7d2f2ef575afea7c6d
ArghadeepRathore <arghadeeprathore2432707@gmail.com>
2026-09-23 12:31:27 +0530

    Add HackerRank Easy - Input and Output

---
+++ HackerRank/Easy/Input and Output/solution.cpp
@@ -0,0 +1,23 @@
+// HackerRank Problem: Input and Output
+// Link: https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+// Difficulty: Easy
+// Language: cpp
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int a, b, c, sum;
+    cin >>a; 
+    cin >>b;
+    cin >>c; 
+    sum = a+b+c;
+    cout <<sum;
+     return 0;
+}
