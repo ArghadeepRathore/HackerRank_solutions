305dbb650af7841c7a79358d2a7f05eb73dd9e87
ArghadeepRathore <arghadeeprathore2432707@gmail.com>
2026-09-23 12:34:25 +0530

    Add HackerRank Easy - Sum and Difference of Two Numbers

---
+++ HackerRank/Easy/Sum and Difference of Two Numbers/solution.c
@@ -0,0 +1,24 @@
+// HackerRank Problem: Sum and Difference of Two Numbers
+// Link: https://www.hackerrank.com/challenges/sum-numbers-c/problem
+// Difficulty: Easy
+// Language: c
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+    float c,d;
+    scanf("%d %d",&a,&b);
+    scanf("%f %f",&c ,&d);
+    int int_sum = a+b;
+    int int_diff = a-b;
+    float float_sum = c+d;
+    float float_diff = c-d;
+    printf("%d %d\n",int_sum,int_diff);
+    printf("%0.1f %0.1f", float_sum,float_diff);
+    return 0;
+}
