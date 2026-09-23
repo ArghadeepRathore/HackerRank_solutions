8d3a6555bfba0287c6b09dabdc7f3a47b54b12cc
ArghadeepRathore <arghadeeprathore2432707@gmail.com>
2026-09-23 12:34:35 +0530

    Add HackerRank Easy - "Hello World!" in C

---
+++ HackerRank/Easy/Hello World! in C/solution.c
@@ -0,0 +1,22 @@
+// HackerRank Problem: "Hello World!" in C
+// Link: https://www.hackerrank.com/challenges/hello-world-c/problem
+// Difficulty: Easy
+// Language: c
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+    printf("Hello, World!\n");
+    printf(s);
+    return 0;
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
