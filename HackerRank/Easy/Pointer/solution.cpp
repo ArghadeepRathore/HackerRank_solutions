8c5c30ceb01e1ec6e01b9ff541c5f83a79ab4a5c
ArghadeepRathore <arghadeeprathore2432707@gmail.com>
2026-09-23 12:30:55 +0530

    Add HackerRank Easy - Pointer

---
+++ HackerRank/Easy/Pointer/solution.cpp
@@ -0,0 +1,32 @@
+// HackerRank Problem: Pointer
+// Link: https://www.hackerrank.com/challenges/c-tutorial-pointer/problem
+// Difficulty: Easy
+// Language: cpp
+
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    int sum = *a+*b;
+    int subtract;
+    if(*a>*b)
+    {
+         subtract = *a-*b;
+    }  
+    else
+    {
+        subtract = *b-*a;
+    }
+     *a = sum;
+   *b = subtract;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
