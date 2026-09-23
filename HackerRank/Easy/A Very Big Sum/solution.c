c948d24132f87a25dab94c6e30ee792a536ae252
ArghadeepRathore <arghadeeprathore2432707@gmail.com>
2026-09-23 12:32:33 +0530

    Add HackerRank Easy - A Very Big Sum

---
+++ HackerRank/Easy/A Very Big Sum/solution.c
@@ -0,0 +1,27 @@
+// HackerRank Problem: A Very Big Sum
+// Link: https://www.hackerrank.com/challenges/a-very-big-sum/problem
+// Difficulty: Easy
+// Language: c
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int j,k,l;
+    scanf("%d",&j);
+    unsigned long long int t[j],r=0;
+    
+    for(k=0;k<j;k++)
+        {
+        scanf("%lld",&t[k]);
+        }
+    for(k=0;k<j;k++)
+        {
+        r=r+t[k];
+        }
+    printf("%lld",r);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
